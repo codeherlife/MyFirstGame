3392f46d5c9fdfaf0190bb11f7559bd95419d2d1
codeherlife <codeherlife@gmail.com>
2018-04-21 22:14:33 -0700

    added constructor with super class and override

--- app/src/main/java/com/codeherlife/myfirstgame/MainThread.java
+++ app/src/main/java/com/codeherlife/myfirstgame/MainThread.java
@@ -1,8 +1,35 @@
 package com.codeherlife.myfirstgame;
 
+import android.graphics.Canvas;
+import android.view.SurfaceHolder;
+
 /**
  * Created by melanie on 4/21/18.
  */
 
 public class MainThread extends Thread {
+
+    private int FPS = 30;
+    private double averageFPS;
+    private SurfaceHolder surfaceHolder;
+    private GamePanel gamePanel;
+    private boolean running;
+    public static Canvas canvas;
+
+    //the constructor, call the constructor with the super class and override the run method inside class thread
+    public MainThread(SurfaceHolder surfaceHolder, GamePanel gamePanel){
+
+        super();
+        this.surfaceHolder = surfaceHolder;
+        this.gamePanel = gamePanel;
+    }
+    @Override
+    public void run(){
+        long startTime;
+        long timeMillis;
+        long waitTime;
+        long totalTime = 0;
+        int frameCount = 0;
+        long targetTime = 1000/FPS;
+    }
 }
