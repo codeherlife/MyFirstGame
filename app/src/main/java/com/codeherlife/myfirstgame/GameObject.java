fdee11009dea677a790b99cdd10a8d43faf8c986
codeherlife <codeherlife@gmail.com>
2018-04-29 17:38:44 -0700

    Added GameObject class and fields

---
+++ app/src/main/java/com/codeherlife/myfirstgame/GameObject.java
@@ -0,0 +1,14 @@
+package com.codeherlife.myfirstgame;
+
+/**
+ * Created by melanie on 4/29/18.
+ */
+
+public abstract class GameObject {
+    protected int x;
+    protected int y;
+    protected int dy;
+    protected int dx;
+    protected int width;
+    protected int height;
+}
