91c9811c5fdc5e7dd953f9d38947fed0ffc72b31
codeherlife <codeherlife@gmail.com>
2018-04-29 18:16:15 -0700

    added Player class and fields

---
+++ app/src/main/java/com/codeherlife/myfirstgame/Player.java
@@ -0,0 +1,38 @@
+package com.codeherlife.myfirstgame;
+
+import android.graphics.Bitmap;
+import android.view.animation.Animation;
+
+/**
+ * Created by melanie on 4/29/18.
+ */
+
+public class Player extends GameObject{
+    private Bitmap spritesheet;
+    private int score;
+    private double dya;
+    private boolean up;
+    private boolean playing;
+    private Animation animation = new Animation();
+    private long startTime;
+
+    public Player(Bitmap res, int w, int h, int numFrames)
+    {
+        x = 100;
+        y = GamePanel.HEIGHT/2;
+        dy = 0;
+        score = 0;
+        height = h;
+        width = w;
+
+        Bitmap[] image = new Bitmap[numFrames];
+
+        for(int i =0; i < image.length; i++)
+        {
+            image[i] = Bitmap.createBitmap(spritesheet, i*width, 0, width, height);
+        }
+
+        animation.setFrames(image);
+        animation.setDelay(10);
+    }
+}
