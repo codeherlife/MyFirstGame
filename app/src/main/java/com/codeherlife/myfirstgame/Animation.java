ca9640afa8e1ed15747b759ab660907b2b262803
codeherlife <codeherlife@gmail.com>
2018-04-30 21:47:01 -0700

    added methods and constructors for animation class

--- app/src/main/java/com/codeherlife/myfirstgame/Animation.java
+++ app/src/main/java/com/codeherlife/myfirstgame/Animation.java
@@ -19,5 +19,28 @@ public class Animation {
         currentFrame = 0;
         startTime = System.nanoTime();
     }
+    public void setDelay(long d){ delay = d;}
+    public void setFrame(int i){ currentFrame = i;}
+
+    public void update()
+    {
+        long elapsed = (System.nanoTime()-startTime)/1000000;
+
+        if(elapsed > delay)
+        {
+            currentFrame++;
+            startTime = System.nanoTime();
+        }
+        if(currentFrame == frames.length)
+        {
+            currentFrame = 0;
+            playedOnce = true;
+        }
+    }
+    public Bitmap getImage(){
+        return frames[currentFrame];
+    }
+    public int getFrame(){ return currentFrame;}
+    public boolean playedOnce(){ return playedOnce;}
 }
-public void setDelay
+
