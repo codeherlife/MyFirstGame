9073f44550f9c96de6a21a2165e5c6c810882cf9
codeherlife <codeherlife@gmail.com>
2018-05-05 10:56:49 -0700

    added to Missile class get set for collision

--- app/src/main/java/com/codeherlife/myfirstgame/Missile.java
+++ app/src/main/java/com/codeherlife/myfirstgame/Missile.java
@@ -1,5 +1,8 @@
 package com.codeherlife.myfirstgame;
 
+import android.graphics.Bitmap;
+import android.graphics.Canvas;
+
 import java.util.Random;
 
 /**
@@ -10,6 +13,9 @@ public class Missile extends GameObject {
     private int score;
     private int speed;
     private Random rand = new Random();
+    private Animation animation = new Animation();
+    private Bitmap spritesheet;
+
 
 
 
@@ -19,12 +25,46 @@ public class Missile extends GameObject {
 
         super.x = x;
         super.y = y;
-
         width = w;
-
         height = h;
+        score = s;
 
+        speed = 7 + (int) (rand.nextDouble()*score/30);
+
+        //cap missile speed
+        if(speed>40){ speed = 40;}
+
+        Bitmap[] image= new Bitmap[numFrames];
+
+        spritesheet = res;
+
+        for(int i= 0; i< image.length; i++){
+
+            image[i] = Bitmap.createBitmap(spritesheet, 0, i*height, width, height);
+        }
+
+        animation.setFrames(image);
+        animation.setDelay(100-speed);
     }
 
+    public void update()
+    {
+        x-=speed;
+        animation.update();
+    }
+
+    public void draw(Canvas canvas)
+    {
+        try{
+            canvas.drawBitmap(animation.getImage(), x,y,null);
+        }catch(Exception e) {}
+    }
+
+    @Override
+    public int getWidth()
+    {
+        //offset slightly for more realistic collision detection
+
+    }
 
 }
