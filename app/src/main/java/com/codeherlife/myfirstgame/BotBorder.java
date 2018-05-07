1562d17e404b48c6c476276fc117d2666dac9561
codeherlife <codeherlife@gmail.com>
2018-05-06 18:08:15 -0700

    added BorBorder class, fields and constructors

--- app/src/main/java/com/codeherlife/myfirstgame/BotBorder.java
+++ app/src/main/java/com/codeherlife/myfirstgame/BotBorder.java
@@ -1,8 +1,32 @@
 package com.codeherlife.myfirstgame;
 
+import android.graphics.Bitmap;
+import android.graphics.Canvas;
+
 /**
  * Created by melanie on 5/6/18.
  */
 
-public class BotBorder {
+public class BotBorder extends GameObject{
+
+    private Bitmap image;
+    public BotBorder(Bitmap res, int x, int y)
+    {
+        height = 200;
+        width = 20;
+
+        this.x = x;
+        this.y = y;
+        dx = GamePanel.MOVESPEED;
+
+        image = Bitmap.createBitmap(res, 0,0,width,height);
+    }
+    public void update()
+    {
+        x+=dx;
+    }
+    public void draw(Canvas canvas)
+    {
+        canvas.drawBitmap(image, x,y,null);
+    }
 }
